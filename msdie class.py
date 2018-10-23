5601ad938c9a855a4993e0961b75530485983fba
ShawntheSun <43038178+ShawntheSun@users.noreply.github.com>
2018-10-23 10:42:50 -0400

    Update msdie class.py

--- msdie class.py
+++ msdie class.py
@@ -43,6 +43,34 @@ class msdie:
         t.pendown()
         t.dot(25)
         t.exitonclick()
+   def side5(self, size, dotsize):
+        t = turtle
+        t.setposition(0, 0)
+        t.hideturtle()
+        for x in range(0, 4):  # loop so it draws 4 sides
+            t.forward(size)  # side
+            t.right(90)  # corner
+        t.penup()
+        t.setposition(size/4, -size/4)  # first dot
+        t.pendown()
+        t.dot(dotsize)  # draw dot
+        t.penup()
+        t.setposition(size/4*3, -size/4)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/2, -size/2)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/4, -size/4*3)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/4*3, -size/4*3)
+        t.pendown()
+        t.dot(dotsize)
+        t.exitonclick()
 die1 = msdie(6)
 die1.roll()
 
@@ -85,41 +113,42 @@ if die1.getValue() == 2:
  #exit the window when the user clicks the screen
 if die1.getValue() == 4:
     die1.drawSide4()
-    
-    if die1.getValue() == 6:
-        t.screensize(500, 500)
-        t.speed(4)
-        t.penup()   # not drawing when moving
-        t.setpos(-100, 100)  # setting up the start position
-        t.pendown()   # drawing when moving
-        for i in range(4):    # drawing the frame of the dice ( a square )
-            t.forward(200)
-            t.right(90)
-        def draw_dots():
-            t.penup()
-            t.setposition(-40, 60)  # drawing the first column of the dots
-            t.setheading(-90)
-            t.pendown()
-            t.dot(30,"blue")
-        for i in range(2):
-            t.penup()
-            t.forward(60)
-            t.pendown()
-            t.dot(30, "blue")
+if die1.getValue() == 5:
+    die1.side5(25, 25)
+if die1.getValue() == 6:
+    t.screensize(500, 500)
+    t.speed(4)
+    t.penup()   # not drawing when moving
+    t.setpos(-100, 100)  # setting up the start position
+    t.pendown()   # drawing when moving
+    for i in range(4):    # drawing the frame of the dice ( a square )
+        t.forward(200)
+        t.right(90)
+    def draw_dots():
         t.penup()
-        t.setposition(35, 60)   # drawing the second column of the dots
+        t.setposition(-40, 60)  # drawing the first column of the dots
         t.setheading(-90)
         t.pendown()
+        t.dot(30,"blue")
+    for i in range(2):
+        t.penup()
+        t.forward(60)
+        t.pendown()
         t.dot(30, "blue")
-       for i in range(2):
-           t.penup()
-           t.forward(60)
-           t.pendown()
-           t.dot(30, "blue")
-       draw_dots()
+    t.penup()
+    t.setposition(35, 60)   # drawing the second column of the dots
+    t.setheading(-90)
+    t.pendown()
+    t.dot(30, "blue")
+   for i in range(2):
+       t.penup()
+       t.forward(60)
+       t.pendown()
+       t.dot(30, "blue")
+   draw_dots()
 
-    t.hideturtle() # make the turtle invisible
-    t.exitonclick() # quit when we choose
+t.hideturtle() # make the turtle invisible
+t.exitonclick() # quit when we choose
 
 # code to test if this is working
 print("die value is: ", die1.getValue())
