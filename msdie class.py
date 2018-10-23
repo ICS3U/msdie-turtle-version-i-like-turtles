80e48f8d776da9e330edea4f87f3f56a5d103cce
ShawntheSun <43038178+ShawntheSun@users.noreply.github.com>
2018-10-23 10:10:08 -0400

    Add files via upload

---
+++ msdie class.py
@@ -0,0 +1,53 @@
+# our first class
+from random import *
+
+
+class msdie:
+    def __init__(self, sides):
+        self.sides = sides
+        self.value = 1
+
+    def roll(self):
+        self.value = randrange(1, self.sides + 1)
+        return self.value
+
+    def getValue(self):
+        return self.value
+
+    def setValue(self, value):
+        self.value = value
+
+    def drawSide4(self):
+        import turtle
+        t = turtle
+        t.penup()
+        t.setpos(-100, 100)
+        t.pendown()
+        for i in range(4):
+            t.forward(200)
+            t.right(90)
+        t.penup()
+        t.setpos(-35, 35)
+        t.pendown()
+        t.dot(25)
+        t.penup()
+        t.setpos(-35, -35)
+        t.pendown()
+        t.dot(25)
+        t.penup()
+        t.setpos(35, 35)
+        t.pendown()
+        t.dot(25)
+        t.penup()
+        t.setpos(35, -35)
+        t.pendown()
+        t.dot(25)
+        t.exitonclick()
+
+# code to test if this is working
+die1 = msdie(6)
+die1.roll()
+# if die1.getValue() == 4:
+die1.drawSide4()
+
+print("die value is: ", die1.getValue())
