90a894a63966f3124d898515964b637f5a9adcd7
DanielNairn <43038308+DanielNairn@users.noreply.github.com>
2018-10-23 10:13:08 -0400

    Update msdie class.py

--- msdie class.py
+++ msdie class.py
@@ -1,8 +1,10 @@
 # our first class
 from random import *
-
+import turtle
 
 class msdie:
+    t = turtle
+    
     def __init__(self, sides):
         self.sides = sides
         self.value = 1
@@ -18,8 +20,6 @@ class msdie:
         self.value = value
 
     def drawSide4(self):
-        import turtle
-        t = turtle
         t.penup()
         t.setpos(-100, 100)
         t.pendown()
@@ -44,10 +44,40 @@ class msdie:
         t.dot(25)
         t.exitonclick()
 
+    def side5(self, size, dotsize):
+        t = turtle
+        t.setposition(0, 0)
+        t.hideturtle()
+        for x in range(0, 4):  # loop so it draws 4 sides
+            t.forward(size)  # side
+            t.right(90)  # corner
+        t.penup()
+        t.setposition(size/4, -size/4)  # first dot
+        t.pendown()
+        t.dot(dotsize)  # draw dot
+        t.penup()
+        t.setposition(size/4*3, -size/4)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/2, -size/2)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/4, -size/4*3)
+        t.pendown()
+        t.dot(dotsize)
+        t.penup()
+        t.setposition(size/4*3, -size/4*3)
+        t.pendown()
+        t.dot(dotsize)
+        t.exitonclick()
+        
 # code to test if this is working
 die1 = msdie(6)
 die1.roll()
 # if die1.getValue() == 4:
 die1.drawSide4()
+die1.side5(200, 20)
 
 print("die value is: ", die1.getValue())
